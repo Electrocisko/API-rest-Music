10c1e5bf10d2f13cfcfeb023345703d1204aea3d
Francisco Zuidwijk <electrocisko@gmail.com>
2023-09-20 16:17:53 -0300

    multer

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -4,7 +4,8 @@ import validate from "../helpers/validations.js";
 import UserDtoPresenter from "../helpers/userDTO.js";
 import jwt from "../services/jwt.js";
 import mongoose from "mongoose";
-// let checkpas = await isValidPassword(passwordHashed,"123" );
+import fs from 'fs';
+
 
 // Acciones de prueb
 const pruebaUser = (req, res) => {
@@ -112,31 +113,88 @@ const updateUser = async (req, res) => {
     let userIdentity = req.user;
     //Recoger datos a actualizar
     let userToUpdated = req.body;
+
     //Comprobar si el usuario existe
-    let existUser = await User.findOne({
+    let users = await User.find({
       $or: [{ email: userToUpdated.email }, { nick: userToUpdated.nick }],
     });
-    if (!existUser) throw new Error("No se ha encontrado el usuario");
+    if (!users) throw new Error("No se ha encontrado el usuario");
     //Comprobar si el usuario existe y no soy yo(el identificado)
-    if (userIdentity._id != existUser._id)
-      throw new Error("No tiene las credenciales para modificar otro usuario");
+    let userIsset = false;
+    users.forEach((user) => {
+      if (user && user._id != userIdentity._id) userIsset = true;
+    });
+    if (userIsset) throw new Error("Ya existe un usuario con ese mail o nick");
 
-    // FALTA VALIDATOR NO?
+    // Validar
+    if (!userToUpdated.nick) throw new Error("Falta Nick");
+    if (!userToUpdated.email) throw new Error("Falta Email");
+    if (!userToUpdated.password) throw new Error("Falta Password");
+    validate(userToUpdated);
 
     //Cifrar password si es que llega
     if (userToUpdated.password) {
       userToUpdated.password = await createHash(userToUpdated.password);
     }
+
     // Buscar usuario en bd  y actualizar
+    let userToSave = await User.findByIdAndUpdate(
+      userIdentity._id,
+      userToUpdated,
+      { new: true }
+    );
 
     //Devolver repuesta
 
     return res.status(200).json({
       status: "sucess",
       message: "Updated user",
-      userIdentity,
-      userToUpdated,
+      user: UserDtoPresenter(userToSave),
     });
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+const loader = async (req, res) => {
+  try {
+    // configurar multer como middleware
+
+    //Recoger fichero de imagen y ver si existe
+    if(!req.file) throw new Error("La petición no incluye un fichero de imagen");
+    //Obtener nombre del archivo
+    let image = req.file.originalname;
+    //obtener la extension del arhivo de la imagen
+    const imageSplit = image.split("\."); //devuele un array y el ultimo elemento es la extension
+    const extension = imageSplit[1];
+    //Chequear la extension
+    if(extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif"){
+      const filePath = req.file.path;
+      fs.unlinkSync(filePath); // borro el archivo incorrecto
+      throw new Error("Extension no valida")
+    }
+    // guardar en base de datos
+    let userUpdated = await User.findByIdAndUpdate(
+      req.user._id,
+      { image: req.file.path },
+      { new: true }
+    );
+
+  
+    
+    //Devolver repuesta.
+
+    return res
+      .status(200)
+      .json({ 
+        status: "success",
+        message: "Subiendo el archivo",
+        file: req.file,
+        userUpdated: UserDtoPresenter(userUpdated)
+       });
   } catch (error) {
     return res.status(400).json({
       status: "error",
@@ -151,4 +209,5 @@ export default {
   loginUser,
   profileUser,
   updateUser,
+  loader,
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,14 +1,17 @@
 import {Router} from "express";
 import userControler from "../controllers/userControler.js";
 import auth from "../middlewares/auth.js";
+import upload from "../middlewares/multer.js"; //Multer
 
 const router = Router();
+
 //Defino las rutas
 router.get('/prueba',userControler.pruebaUser );
 router.post('/register',userControler.registerUser);
 router.post('/login',userControler.loginUser);
 router.get('/profile/:id',auth, userControler.profileUser);
 router.put('/updated',auth, userControler.updateUser);
+router.post('/upload', [auth, upload.single("avatar")], userControler.loader);
 
 
 export default router;
