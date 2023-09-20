94791b9982d38f333578f9f00394d72394a3bc8c
Francisco Zuidwijk <electrocisko@gmail.com>
2023-09-20 17:06:56 -0300

    obtener avatar de usuario

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -4,8 +4,8 @@ import validate from "../helpers/validations.js";
 import UserDtoPresenter from "../helpers/userDTO.js";
 import jwt from "../services/jwt.js";
 import mongoose from "mongoose";
-import fs from 'fs';
-
+import fs from "fs";
+import path from "path";
 
 // Acciones de prueb
 const pruebaUser = (req, res) => {
@@ -164,17 +164,23 @@ const loader = async (req, res) => {
     // configurar multer como middleware
 
     //Recoger fichero de imagen y ver si existe
-    if(!req.file) throw new Error("La petición no incluye un fichero de imagen");
+    if (!req.file)
+      throw new Error("La petición no incluye un fichero de imagen");
     //Obtener nombre del archivo
     let image = req.file.originalname;
     //obtener la extension del arhivo de la imagen
-    const imageSplit = image.split("\."); //devuele un array y el ultimo elemento es la extension
+    const imageSplit = image.split("."); //devuele un array y el ultimo elemento es la extension
     const extension = imageSplit[1];
     //Chequear la extension
-    if(extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif"){
+    if (
+      extension != "png" &&
+      extension != "jpg" &&
+      extension != "jpeg" &&
+      extension != "gif"
+    ) {
       const filePath = req.file.path;
       fs.unlinkSync(filePath); // borro el archivo incorrecto
-      throw new Error("Extension no valida")
+      throw new Error("Extension no valida");
     }
     // guardar en base de datos
     let userUpdated = await User.findByIdAndUpdate(
@@ -182,19 +188,12 @@ const loader = async (req, res) => {
       { image: req.file.path },
       { new: true }
     );
-
-  
-    
-    //Devolver repuesta.
-
-    return res
-      .status(200)
-      .json({ 
-        status: "success",
-        message: "Subiendo el archivo",
-        file: req.file,
-        userUpdated: UserDtoPresenter(userUpdated)
-       });
+    return res.status(200).json({
+      status: "success",
+      message: "Subiendo el archivo",
+      file: req.file,
+      userUpdated: UserDtoPresenter(userUpdated),
+    });
   } catch (error) {
     return res.status(400).json({
       status: "error",
@@ -203,6 +202,30 @@ const loader = async (req, res) => {
   }
 };
 
+const avatar = async (req, res) => {
+  try {
+    const file = req.params.file;
+    const filePath = "./src/uploads/avatars/"+file;
+    // Para comprobar si el archivo existe uso el metodo stat de file sysytem.
+    fs.stat(filePath, (error, exsist) => {
+      if (error || !exsist) {
+        return res.status(400).json({
+          status:"error",
+          message:"No se encontro archivo"
+        })
+      }
+    })
+    // En lugar de devolver un json devuelvo un file
+    return res.sendFile(path.resolve(filePath))
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+
 export default {
   pruebaUser,
   registerUser,
@@ -210,4 +233,5 @@ export default {
   profileUser,
   updateUser,
   loader,
+  avatar,
 };
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,6 +12,7 @@ router.post('/login',userControler.loginUser);
 router.get('/profile/:id',auth, userControler.profileUser);
 router.put('/updated',auth, userControler.updateUser);
 router.post('/upload', [auth, upload.single("avatar")], userControler.loader);
+router.get('/avatar/:file',userControler.avatar);
 
 
 export default router;
